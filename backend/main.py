e1e7fb39843fd5fb599ee11391b4b57cf5ac9694
YNR <tdmb077747@gmail.com>
2026-07-10 13:56:50 +0530

    Barebones backend - feeds user input into local host db

---
+++ backend/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import Form
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
+import asyncpg
+
+load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.conn = await asyncpg.connect(
+        host="localhost",
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSW"),
+        database="myapp",
+    )
+
+    await app.state.conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(20) NOT NULL,
+        password VARCHAR(20) NOT NULL
+        )
+        """
+    )
+
+    yield
+
+    await app.state.conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/static", StaticFiles(directory="frontend/static"))
+
+
+@app.get("/")
+async def webpage():
+    return FileResponse("frontend/index.html")
+
+
+@app.post("/user_reg")
+async def reg(username: str = Form(...), password: str = Form(...)):
+    await app.state.conn.execute(
+        """
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            """,
+        username,
+        password,
+    )
+
+    return {"message": "User Registered!"}
